30713a9374dd5163c2905084c77173301079d8b2
John Nguyen-Tran <johnnyvng@gmail.com>
2026-03-19 00:09:59 -0400

    first commit on chap 2

---
+++ Chap2.ps1
@@ -0,0 +1,11 @@
+﻿New-Item -ItemType Directory $env:USERPROFILE\Apps
+
+setx /m PATH "$env:PATH;$env:USERPROFILE\Apps"
+
+Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -All -NoRestart
+
+podman run --rm hello-world
+
+docker run --rm hello-world
+
+Set-Alias -Name docker -Value podman
